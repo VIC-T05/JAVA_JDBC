0bbf0ae6b1efd513db4a60392d9c58e5111fa5be
unknown <victordzns1@gmail.com>
2024-11-12 16:58:02 -0300

    findByDepartmentId implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,18 @@
 package application;
 
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-
-		Department obj = new Department(1, "books");
 		
-		System.out.println(obj);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		System.out.println("=== TEST 1: seller findById =====");	
+		Seller seller = sellerDao.findById(3);
+		System.out.println(seller);
 		
-
 	}
 
 }
